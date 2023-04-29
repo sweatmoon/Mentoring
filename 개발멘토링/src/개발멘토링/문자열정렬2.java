97782285228ffd0f5ceb1c77503128107234f696
sweat <sweat@192.168.123.159>
2023-04-29 19:06:55 +0900

    2023-04-29

--- 개발멘토링/src/개발멘토링/문자열정렬2.java
+++ 개발멘토링/src/개발멘토링/문자열정렬2.java
@@ -1,50 +1,36 @@
 package 개발멘토링;
 
 public class 문자열정렬2 {
-    public static void main(String[] args) {
-        
-        String temp;
-        String Arr[] = { "김민정", "김종훈", "김지훈", "김채은", "김태호", "김현선", "김현호", "노은영", "유성희", "이태환", "이현우", "정혜정", "조영호", "조현영", "홍원상",
-                        "강애정", "김수엽", "김정훈", "류경숙", "박범진", "박승진", "박정근", "양권묵", "이운주", "이지은", "장덕호", "최성욱", "최정욱", "최지윤", "황극인" };
-        String[] ASC = new String[Arr.length];
-        String[] DESC = new String[Arr.length];
+	public static void main(String[] args) {
 
-        for (int inx = 0; inx < Arr.length; inx++) {
-            for (int jnx = inx + 1; jnx < Arr.length; jnx++) {
-                if (Arr[inx].compareTo(Arr[jnx]) > 0){
-                    temp = Arr[inx];
-                    Arr[inx] = Arr[jnx];
-                    Arr[jnx] = temp;
-                }
-            }
-            ASC[inx] = Arr[inx];
-        } 
+		String arr[] = { "김민정", "김종훈", "김지훈", "김채은", "김태호", "김현선", "김현호", "노은영", "유성희", "이태환", "이현우", "정혜정", "조영호",
+				"조현영", "홍원상", "강애정", "김수엽", "김정훈", "류경숙", "박범진", "박승진", "박정근", "양권묵", "이운주", "이지은", "장덕호", "최성욱", "최정욱",
+				"최지윤", "황극인" };
 
-        for (int inx = 0; inx < Arr.length; inx++) {
-            for (int jnx = inx + 1; jnx < Arr.length; jnx++) {
-                if (Arr[inx].compareTo(Arr[jnx]) < 0){
-                    temp = Arr[inx];
-                    Arr[inx] = Arr[jnx];
-                    Arr[jnx] = temp;
-                }
-            }
-            DESC[inx] = Arr[inx];
-        }
+		sortArray(arr);
+		printArray(arr);
 
-        System.out.print("오름차순 : ");
-        for (int inx = 0; inx < ASC.length; inx++) {
-            System.out.print(ASC[inx]);
-            if (inx < ASC.length - 1){
-                System.out.print(", ");
-            }
-        } System.out.println();
-       
-       System.out.print("내림차순 : ");
-        for (int inx = 0; inx < DESC.length; inx++) {
-            System.out.print(DESC[inx]);
-            if (inx < DESC.length - 1){
-                System.out.print(", ");
-            }
-        } System.out.println();
-    }
+	}
+
+	public static void sortArray(String[] arr) {
+		String temp;
+		for (int inx = 0; inx < arr.length - 1; inx++) { // 버블정렬 시 로직의 논리적 정확성을 위해 배열길이 - 1 까지
+			for (int jnx = inx + 1; jnx < arr.length; jnx++) {
+				if (arr[inx].compareTo(arr[jnx]) > 0) {
+					temp = arr[inx];
+					arr[inx] = arr[jnx];
+					arr[jnx] = temp;
+				}
+			}
+		}
+	}
+
+	public static void printArray(String[] arr) {
+		for (int inx = 0; inx < arr.length; inx++) {
+			System.out.print(arr[inx]);
+			if (inx < arr.length - 1) {
+				System.out.print(", ");
+			}
+		}
+	}
 }
